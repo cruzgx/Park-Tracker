4d29c9ebf5a9af6009272b8019b4fb1ebd9025f6
Cruz Gonzalez <cruzgonzalez888@gmail.com>
2021-07-09 17:32:24 -0400

    Added homepage to webstie
    Added react router dom and set up the linking of other websites

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+// Imports here
+import React from "react";
+
+// Props can be passed to home component
+const Home = () => {
+
+    return(
+        <div>
+           <h1>Hello world</h1>
+        </div>
+    );
+}
+
+export default Home;
