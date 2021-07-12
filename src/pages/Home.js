d21b0a60e81df6e31e24548592dfe73c2bf67496
Cruz Gonzalez <cruzgonzalez888@gmail.com>
2021-07-12 18:04:55 -0400

    Added basic styling to the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,26 @@
 // Imports here
 import React from "react";
 import { Link } from "react-router-dom";
+import '../styling/Home.css'
 
 // Props can be passed to home component
 const Home = () => {
 
-    return(
+    return (
         <div>
-           <h1>I'm the home page!</h1>
-           <Link to='/about'>Click me to go to the About page</Link>
+            <div className='home_container'>
+                <h1>I'm the home page!</h1>
+                <Link to='/about'>Click me to go to the About page</Link>
+
+                <div className='home_top_imgage' className='home_img_container'>
+                    <h1 className='home_front_decript'>Find your next park</h1>
+                    <img src='https://media.timeout.com/images/100469525/image.jpg' alt='' />
+                </div>
+
+
+                <h1 className='home_info_blurb'>Find out all you need to know any of any of NYC's parks here</h1>
+
+            </div>
         </div>
     );
 }
---
+++ src/pages/ParkSearch.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+//props can be added here
+const ParkSearch = () => {
+    //
+}
+
+export default ParkSearch
