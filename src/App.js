99db30008347f537f65f782d3fc747b6b17af3e4
Cruz Gonzalez <cruzgonzalez888@gmail.com>
2021-07-12 12:33:48 -0400

    Added about page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home'
+import About from './pages/About';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path='/' exact component={Home} />
+        <Route path='/about' exact component={About} />
       </Switch>
     </BrowserRouter>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 // Imports here
 import React from "react";
+import { Link } from "react-router-dom";
 
 // Props can be passed to home component
 const Home = () => {
 
     return(
         <div>
-           <h1>Hello world</h1>
+           <h1>I'm the home page!</h1>
+           <Link to='/about'>Click me to go to the About page</Link>
         </div>
     );
 }
