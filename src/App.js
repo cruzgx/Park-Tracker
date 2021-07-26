9e26a94ae9ed44bcfe3c0fcef5f9901877b54b6f
Cruz Gonzalez <cruzgonzalez888@gmail.com>
2021-07-26 11:27:24 -0400

    Added a image slide to front page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home'
 import About from './pages/About';
+import { Layout } from './components/Layout.js';
+import { NavigationBar } from './components/NavigationBar.js';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path='/' exact component={Home} />
-        <Route path='/about' exact component={About} />
-      </Switch>
-    </BrowserRouter>
+    <React.Fragment>
+      <NavigationBar />
+      <Layout>
+        <Router>
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/about' exact component={About} />
+          </Switch>
+        </Router>
+      </Layout>
+    </React.Fragment>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,70 @@
 // Imports here
 import React from "react";
 import { Link } from "react-router-dom";
-import '../styling/Home.css'
+import "../styling/Home.css";
+import { Carousel } from "react-bootstrap";
 
 // Props can be passed to home component
 const Home = () => {
+  return (
+    <div>
+      <div className="home_container">
+        <Carousel>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="https://cdn.getyourguide.com/img/location/556ddd302d7c4.jpeg/88.jpg"
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>Battery Park</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="https://media.tacdn.com/media/attractions-splice-spp-674x446/0b/27/58/b7.jpg"
+              alt="Second slide"
+            />
 
-    return (
-        <div>
-            <div className='home_container'>
-                <h1>I'm the home page!</h1>
-                <Link to='/about'>Click me to go to the About page</Link>
+            <Carousel.Caption>
+              <h3>Central Park</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="https://www.nycgo.com/images/articles/9587/union-square-julienne-schaer-012__large.jpg"
+              alt="Third slide"
+            />
 
-                <div className='home_top_imgage' className='home_img_container'>
-                    <h1 className='home_front_decript'>Find your next park</h1>
-                    <img src='https://media.timeout.com/images/100469525/image.jpg' alt='' />
-                </div>
+            <Carousel.Caption>
+              <h3>Union Square Park</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
 
+        <Link to="/about">Click me to go to the About page</Link>
 
-                <h1 className='home_info_blurb'>Find out all you need to know any of any of NYC's parks here</h1>
+        {/* <div className="home_top_imgage" className="home_img_container">
+          <h1 className="home_front_decript">Find your next park</h1>
+          <img
+            src="https://media.timeout.com/images/100469525/image.jpg"
+            alt=""
+          />
+        </div> */}
 
-            </div>
-        </div>
-    );
-}
+        <h1 className="home_info_blurb">
+          Find out all you need to know any of any of NYC's parks here
+        </h1>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
