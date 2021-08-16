2975611a83bf669bf6ea72ea9ff1ef943e5cf699
Cruz Gonzalez <cruzgonzalez888@gmail.com>
2021-08-16 14:38:43 -0400

    Added imageto Battery Park page

--- src/pages/indiv_park_pages/Battery.js
+++ src/pages/indiv_park_pages/Battery.js
@@ -1,15 +1,24 @@
 import axios from "axios";
+import Figure from 'react-bootstrap/Figure'
 
 const Battery = (props) => {
     return(
         <div>
-
-            <h1>Battery Park</h1>
-
+            <h1 className='text-center'>Battery Park</h1>
+            <div className='text-center'>
+                <Figure>
+                    <Figure.Image
+                        width={1350}
+                        height={1000}
+                        alt="171x180"
+                        src="https://cdn.getyourguide.com/img/location/556ddd302d7c4.jpeg/88.jpg"
+                    />
+                <Figure.Caption>
+                   A typical day at Battery Park during NYC's summer months
+                </Figure.Caption>
+                </Figure>             
+            </div>
             <button onClick={getWeather}>Click me to get API info.</button>
-            
-
-
         </div>
     )
 }
@@ -17,12 +26,19 @@ const Battery = (props) => {
 const getWeather = () => {
     // fetch('https://api.airvisual.com/v2/nearest_city?lat=40.702772&lon=-74.015545&key=bcf058ea-9c3a-424e-a180-33ccd15e41cd')
     //     .then(responce => responce.json())
-    //     .then(data => console.log(data));
+    //     .then(data => console.log(data))
+    //     let weatherData = data.data;
+    //     console.log(weatherData);
+    //     console.log(weatherData.city)
 
     axios.get('https://api.airvisual.com/v2/nearest_city?lat=40.702772&lon=-74.015545&key=bcf058ea-9c3a-424e-a180-33ccd15e41cd')
         .then(responce => {
-            let weatherData = responce.data;
+            console.log(responce)
+            // console.log(typeof responce)
+            let weatherData = responce.data.data;
             console.log(weatherData)
+            console.log(typeof weatherData)
+            console.log(weatherData.city)
         })
 
 
